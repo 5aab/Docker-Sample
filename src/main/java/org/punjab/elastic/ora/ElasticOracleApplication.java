07f089b5a91ffcaabf7bad4277af2fe449fa44ee
Ish Mahajan <ishmahajanbhanu@gmail.com>
2020-09-23 10:12:08 +0530

    docker sample

---
+++ src/main/java/org/punjab/elastic/ora/ElasticOracleApplication.java
@@ -0,0 +1,14 @@
+package org.punjab.elastic.ora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ElasticOracleApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(ElasticOracleApplication.class, args);
+    }
+
+}
